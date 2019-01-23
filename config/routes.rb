444dcb80a0276409a97d533ae50f40cb49297ed5
Marc <akbar13@live.com>
2019-01-23 14:36:14 -0700

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  resources :articles
+  # Created:
+    # A new article path
+    # A post to create articles
+    # An edit article path
+    # A patch to update articles
+    # Delete article path
+    # Show article path
+    # Index article path
+
+  # Priority is based on order of creation: first -> highest priority
 end

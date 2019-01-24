78d80bd0099a435f2a2e68977ec2a9d5a91789a5
Marc <akbar13@live.com>
2019-01-23 18:30:13 -0700

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,18 @@ class ArticlesController < ActionController::Base
   end
 
   def create
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    # Conventional syntax
+    if @article.save
+      flash[:notice] = "Article was succefully created"
+      redirect_to article_path(@article)
+    else
+      render 'new' # or :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  # Priority is based on order of creation: first -> highest priority
+  
   # Changes root page to the home page
   root 'pages#home'
   get 'about', to: 'pages#about'
@@ -14,5 +15,4 @@ Rails.application.routes.draw do
     # Show article path
     # Index article path
 
-  # Priority is based on order of creation: first -> highest priority
 end

93e0ed0ab450e84407ba6dc0b51b5397dc5ccdc4
Marc <akbar13@live.com>
2019-03-01 09:38:19 -0700

    Link from home thumbnail to article active

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,3 +7,27 @@ class PagesController < ApplicationController
   end
 
 end
+
+
+
+# pages#about
+#
+# @three_key_articles = []
+# @three_key_articles << Article.find_by(name: LLamas doing extreme shit)
+# @three_key_articles << Article.find_by(name: alpine llamas extreme)
+# @three_key_articles << Article.find_by(name: llamas drinking caffeine)
+#
+#
+#
+#
+#
+# def about
+#  render about.html.erb
+# end
+#
+# <div class="articles">
+# <ul>
+# <% @three_key_articles.each do |article| %>
+#  <%= <a href="#{article.id}"> <li>article.name<li><a> %>
+# <% end %>
+# </ul>

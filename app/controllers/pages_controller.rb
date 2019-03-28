c8b52d9b826e0c819d224e3e729bc67965e4ea8f
Marc <akbar13@live.com>
2019-03-27 23:18:01 -0600

    Cleaned up some hardcoding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,21 +7,3 @@ class PagesController < ApplicationController
   end
 
 end
-
-
-
-# pages#home
-#
-# @three_key_articles = []
-# @three_key_articles << Article.find_by(title: "Via Ferrata in Innsbruck")
-# @three_key_articles << Article.find_by(title: "Jumping Off Stuff")
-# @three_key_articles << Article.find_by(title: "The Epic Cows of Scotland")
-#
-#
-# <div class="articles">
-#   <ul>
-#     <% @three_key_articles.each do |article| %>
-#       <%= <a href="#{article.id}"> <li>article.name<li><a> %>
-#     <% end %>
-#   </ul>
-# </div>
